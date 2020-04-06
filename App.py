17176ac7ece5bd4053fdb64411170e9ba8a76943
Rita-Sh <61317949+Rita-Sh@users.noreply.github.com>
2020-04-06 23:49:53 +0300

    Update App.py

--- App.py
+++ App.py
@@ -1,36 +1,26 @@
-from flask import Flask, request, render_template
-from datetime import datetime
-import pytz
-
-
+from flask import Flask, request, render_template, jsonify
+import datetime
+from pytz import timezone
 app = Flask(__name__)
-
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route('/moscow')
-def datetime.now(tz=city_tz):
-        city_tz=pytz.timezone('Europe/Moscow')
-        return render_template("time.html", city='Moscow', time=city_tz.strftime('%T'))
-@app.route('/berlin')
-def datetime.now(tz=city_tz):
-        city_tz=pytz.timezone('Europe/Berlin')
-        return render_template("time.html", city='Berlin', time=city_tz.strftime('%T'))
-@app.route('/tokio')
-def datetime.now(tz=city_tz):
-        city_tz=pytz.timezone('Asia/Tokio')
-        return render_template("time.html", city='Tokio', time=city_tz.strftime('%T'))
-@app.route('/newyork')
-def datetime.now(tz=city_tz):
-        city_tz=pytz.timezone('America/New York')
-        return render_template("time.html", city='New York', time=city_tz.strftime('%T'))
+@app.route('/<city>')
+def time(city):
+    if city == 'moscow':
+        zone_time = datetime.datetime.now('Europe/Moscow')
+        return render_template("time.html", city=city, time=zone_time.strftime('%H:%M'))
+    if city == 'berlin':
+        zone_time = datetime.datetime.now('Europe/Berlin')
+        return render_template("time.html", city=city, time=zone_time.strftime('%H:%M'))
+    if city == 'tokyo':
+        zone_time = datetime.datetime.now('Asia/Tokyo')
+        return render_template("time.html", city=city, time=zone_time.strftime('%H:%M'))
+    if city == 'newyork':
+        zone_time = datetime.datetime.now('America/New_York')
+        return render_template("time.html", city=city, time=zone_time.strftime('%H:%M'))
+    else:
+        return render_template("not_found.html")
 
-@app.route('/api', methods = ['GET'])
-def getTime():
-      error = None
-      if request.method == 'GET':
-         searchword=request.args.get('city', ' ' )
-      else:
-          return render_template("not_found.html")
-def get_tasks(city_tz):
-    return jsonify({"city": 'city', "hour": datetime.datetime.now(city_tz).hour , "minute": datetime.datetime.now(city_tz).minute})
+if __name__ == '__main__':
+    app.run()
